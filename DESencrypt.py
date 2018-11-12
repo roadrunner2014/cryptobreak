1373ebdd6f624dab2f7e4995c397b3014d756884
roadrunner2014 <jmc@crydermanenterprises.com>
2018-11-12 07:12:09 -0800

    updated file

--- DESencrypt.py
+++ DESencrypt.py
@@ -1,21 +1,32 @@
 """ Application to encrypt data with DES """
 
 from Crypto.Cipher import DES
-obj=DES.new('abcdefgh', DES.MODE_ECB)
-plain="Guido van Rossum is a space alien."
+from numpy  import random
+
+#plain = str(random.randint(0,9999999999999999))
+plain = b'0123abcd'
+
+#plain = "Guido van Rossum is a space alien."
 
 plaintextsize = len(plain)
 
-# Data padding if input data is not a multiple of 8
+key = b'beefbeef'
+iv = b'abcdefgh'
+cipher = DES.new(key, DES.MODE_OFB)
 
+
+# Data padding if input data is not a multiple of 8
 def padlenght(x):
-    pad = 0
-if multipleof8 % 8 == True:
-    for n in range(0, multipleof8):
-    pad += 'X'
+    pad = ''
+    if multipleof8 % 8 == False:
+        for n in range(0, multipleof8):
+            pad += 'X'
+    else:
+        pad = ''
     return pad
 
 plaintext = plain + pad
 
-ciphertext = obj.encrypt(plain)
-
+#ciphertext = cipher.iv + cipher.encrypt(plaintext)
+ciphertext = cipher.encrypt(plaintext)
+print ciphertext
