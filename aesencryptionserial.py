f3ae7bf9fbe0615a8554e28e388272e8297fbd85
roadrunner2014 <jmc@crydermanenterprises.com>
2018-11-27 18:10:00 -0800

    AES encryption of text files

--- aesencryptionserial.py
+++ aesencryptionserial.py
@@ -1,11 +1,15 @@
 from Crypto.Cipher import AES
 from Crypto import Random
+from mpi4py import MPI
 import os
 import sys
 import time
 
-
 start = time.time()
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
 
 key = b'Sixteen byte key'
 iv = Random.new().read(AES.block_size)
@@ -24,4 +28,4 @@ msg = iv + cipher.encrypt(inputdata)
 #print msg
 
 end = time.time()
-print "Time to compute = ", (end-start)
+print "Time to compute = ", (end-start), "for processor rank = ", rank
