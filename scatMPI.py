9fd85c19b44c7f2e090dad5c3dce4ab11802ca57
RabbitUTSA <42632231+RabbitUTSA@users.noreply.github.com>
2018-12-03 20:22:30 -0600

    Add files via upload

--- scatMPI.py
+++ scatMPI.py
@@ -4,7 +4,7 @@ Created on Wed Nov 28 00:52:36 2018
 
 @author: Rabbit
 """
-
+"""
 from Crypto.Cipher import AES
 from Crypto import Random
 from mpi4py import MPI
@@ -50,12 +50,12 @@ for x in range(numfiles):
 # gather results
 #comm.Gatherv(msg, inputdata, rank=0)
 # do something with result
-"""
+
 if rank == 0:
     print (result)
 else:
     result = None
-"""
+
 # Details on number and size of files
 print ("Number of files encrypted = ", x)
 print ("Total Size of file(s) encypted =  ", totalsize)
@@ -66,3 +66,56 @@ print ("Total Size of file(s) encypted =  ", totalsize)
 
 end = time.time()
 print ("Time to compute = ", (end-start), "for processor rank = ", rank)
+"""
+from Crypto.Cipher import AES
+from Crypto import Random
+from mpi4py import MPI
+import numpy as np
+import os
+import time
+
+start = time.time()
+
+# OpenMPI setup
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+# AES encryption details
+key = b'Sixteen byte key'
+iv = Random.new().read(AES.block_size)
+cipher = AES.new(key, AES.MODE_CFB, iv)
+
+
+# File(s) to encrypt
+# Assume number of files = size (i.e. number of processes)
+numfiles = 10
+totalsize = os.path.getsize('shakespeare.txt')
+inputArray = np.fromfile(open("shakespeare.txt"))
+count = 1
+output = np.empty()
+comm.Scatterv(inputArray, output, root=0)
+if rank == 0:
+    for i in range(1, numfiles):
+        filedata = open("shakespeare.txt", "r")
+        inputdata = filedata.read() + 'This is the end of the file'
+        comm.send(inputdata, dest=i, tag=11)
+        count += 1
+        totalsize += os.path.getsize('shakespeare.txt')
+else:
+    inputdata = comm.recv(source=0, tag=11)
+
+# Local computation of encryption
+msg = iv + cipher.encrypt(inputdata)
+totalsize += os.path.getsize('shakespeare.txt')
+print ("encryption done on process ", rank)
+
+# Wait until all processes are done
+comm.Barrier()
+
+if rank == 0:
+    # Details on number and size of files
+    print ("Number of files encrypted = ", count)
+    print ("Total Size of file(s) encypted =  ", totalsize)
+    end = time.time()
+    print ("Time to compute = ", (end-start), "using point-to-point communication")
